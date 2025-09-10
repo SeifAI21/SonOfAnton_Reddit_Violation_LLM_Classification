13e236ad80499fe09a89a3665a148b32b20f3d2d
SeifAI21 <seifamga447@gmail.com>
2025-09-10 13:58:10 +0300

    fix: Correct dependencies and update training config

--- src/constants.py
+++ src/constants.py
@@ -3,16 +3,17 @@ from transformers.utils import is_torch_bf16_gpu_available
 
 # Main configuration parameters
 WANDB = False  # Enable/disable Weights & Biases logging
-MODEL_NAME = "Qwen/Qwen2.5-7B-Instruct-GPTQ-Int4"
+MODEL_NAME = "Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4"
 IS_DEBUG = True
 N_FOLDS = 5
-EPOCH = 3
+EPOCH = 1
 LR = 1e-4
-TRAIN_BS = 1
-GRAD_ACC_NUM = 8
+TRAIN_BS = 8
+GRAD_ACC_NUM = 1
 EVAL_BS = 8
 FOLD = 0
 SEED = 42
+
 
 # Paths
 EXP_ID = "jigsaw-lora-finetune-baseline"
